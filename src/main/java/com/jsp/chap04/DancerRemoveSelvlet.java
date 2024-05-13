0b9de01d9cdbd03b7aeb1bf0e56e8376240243c4
junglimit <jaehan_jjh@naver.com>
2024-05-13 12:33:51 +0900

    Redirect

--- src/main/java/com/jsp/chap04/DancerRemoveSelvlet.java
+++ src/main/java/com/jsp/chap04/DancerRemoveSelvlet.java
@@ -3,12 +3,14 @@ package com.jsp.chap04;
 import com.jsp.entity.Dancer;
 import com.jsp.repository.DancerJdbcRepo;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/chap04/remove")
 public class DancerRemoveSelvlet extends HttpServlet {
@@ -27,6 +29,10 @@ public class DancerRemoveSelvlet extends HttpServlet {
 
         // db에 삭제명령
         repo.delete(id);
+
+
+        // /chap04/show-list 요청을 자동으로 보냄 ( 리다이렉션 )
+        resp.sendRedirect("/chap04/show-list");
 
     }
 }
